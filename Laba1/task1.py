e118f9df557dacac729181b322a4a0aa6b485636
AinurZR <mesutmccallister@gmail.com>
2024-11-20 10:20:24 +0300

    Update task1.py

--- Laba1/task1.py
+++ Laba1/task1.py
@@ -1,9 +1,13 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-sum_of_numbers =  sum(filter(None, numbers))
-count_of_numbers = len(numbers)
-average_of_numbers = sum_of_numbers / count_of_numbers
-numbers[4] = average_of_numbers
+numbers_sum = 0
+for n in numbers:
+    try:
+        numbers_sum += n
+    except TypeError:
+        continue
+average = numbers_sum / len(numbers)
+numbers[numbers.index(None)] = average
 print("Измененный список:", numbers)
 
